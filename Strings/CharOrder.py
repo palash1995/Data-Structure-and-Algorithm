14c78dbadbf2a026966ee6a36d7ea7d6106fcf80
palash1995 <138295922+palash1995@users.noreply.github.com>
2023-09-21 01:40:05 -0700

    Update CharOrder.py

--- Strings/CharOrder.py
+++ Strings/CharOrder.py
@@ -10,3 +10,35 @@ print(ord("a"))
 print(ord("A"))
 print(chr(97))
 print(chr(65))
+
+
+str = "San Francisco"
+print(str)
+print(str[0])
+print(str[-1])
+
+
+#String is immutable
+# s = "Geek"
+# s[0]="e"
+# print(s)
+
+#Triple quote 
+
+s = """Hello,
+       How are you doing today!
+       Thanks for the head-up"""
+print(s)
+
+#printing new line
+print("Hi John, \nHow are you today!")
+
+#Escape sequence
+print("How are \ you..")
+#print("How are you \") - gives error
+print("How are you \\")
+
+#Raw String
+print("c:\project\name.py") #\n treat as new line..
+s = r"c:\project\name.py"
+print(s)
