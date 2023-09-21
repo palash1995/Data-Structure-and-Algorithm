097abb3a6feb4ec213ea671509499782a4284a90
palash1995 <138295922+palash1995@users.noreply.github.com>
2023-09-20 23:35:00 -0700

    Add files via upload

---
+++ Strings/CharOrder.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 23:31:01 2023
+
+@author: palashchaudhari
+"""
+
+print(ord("a"))
+print(ord("A"))
+print(chr(97))
+print(chr(65))
