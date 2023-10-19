20fc5075197055856a62559f34713362fbabe2d7
palash1995 <138295922+palash1995@users.noreply.github.com>
2023-10-18 18:27:51 -0700

    Add files via upload

---
+++ Queue/QueueLinkList.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 15:19:16 2023
+
+@author: palashchaudhari
+"""
+
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+        
+    def __str__(self):
+        return str(self.value)
+    
+class linkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def __iter__(self):
+        curNode = self.head
+        while curNode:
+            yield curNode
+            curNode = curNode.next
+
+class Queue:
+    def __init__(self):
+        self.linkedList = linkedList()
+    
+    def __str__(self):
+        values = [str(x) for x in self.linkedList]
+        return ' '.join(values)
+    
+    def enqueue(self, value):
+        newNode = Node(value)
+        if self.linkedList.head == None:
+            self.linkedList.head = newNode
+            self.linkedList.tail = newNode
+        else:
+            self.linkedList.tail.next = newNode
+            self.linkedList.tail = newNode
+            
+    def isEmpty(self):
+        if self.linkedList.head == None:
+            return True
+        else:
+            return False
+        
+    def dequeue(self):
+        if self.isEmpty():
+            return "There is no any Node in the Queue..."
+        else:
+            tempNode = self.linkedList.head
+            if self.linkedList.head == self.linkedList.tail:
+                self.linkedList.head = None
+                self.linkedList.tail = None
+            else:
+                self.linkedList.head = self.linkedList.head.next
+            return tempNode
+        
+    def peek(self):
+        if self.isEmpty():
+            return "There is no any Node in the Queue..."
+        else:
+            return self.linkedList.head
+            
+    def delete(self):
+        self.linkedList.head = None
+        self.linkedList.tail = None
+            
+custQueue = Queue()
+custQueue.enqueue(1)
+custQueue.enqueue(2)
+custQueue.enqueue(3)
+print(custQueue.dequeue())
+print(custQueue.peek())
+print(custQueue)
