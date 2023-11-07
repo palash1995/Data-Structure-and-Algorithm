cc618fe9178aeedd31d17ba084d9f32a50e0f6c9
palash1995 <138295922+palash1995@users.noreply.github.com>
2023-11-06 16:26:43 -0800

    Update BinarySearch.py

--- Searching/BinarySearch.py
+++ Searching/BinarySearch.py
@@ -6,3 +6,25 @@ Created on Wed Sep 13 21:05:14 2023
 @author: palashchaudhari
 """
 
+import math
+def binarySearch(array, value):
+    start = 0
+    end = len(array)-1
+    middle = math.floor((start+end)/2)
+    while not(array[middle]==value) and start<=end:
+        if value < array[middle]:
+            end = middle - 1
+        else:
+            start = middle + 1 
+        middle = math.floor((start+end)/2)
+        # print(start, middle, end)
+    if array[middle] == value:
+        return middle
+    else:
+        return -1
+        
+
+
+
+custArray = [8, 9, 12, 15, 17, 19, 20, 21, 28]
+print(binarySearch(custArray, 15))
